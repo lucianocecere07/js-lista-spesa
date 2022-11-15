7dd55ffceb62a41e11cae0797112ad7a2b07620a
lucianocecere07 <cecereluciano97@gmail.com>
2022-11-15 14:09:07 +0100

    stampa elementi con ciclo for

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
 /*Data una lista della spesa già compilata nel vostro codice, stampare in console gli elementi della lista individualmente, prima con un ciclo for, poi con un ciclo while.
 Quando le stampe in console sono corrette, stampare gli elementi in un elenco puntato in HTML, utilizzando uno dei due cicli scritti al punto precedente.*/
 
+let arraySpesa = ["latte", "uova", "mele", "pasta", "arance"];
 
+for(i=0; i<arraySpesa.length; i++){
+    console.log("elemento da comprare: " + arraySpesa[i]);
+}
