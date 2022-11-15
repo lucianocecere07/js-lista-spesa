ce4affecfaa82ff94c3bd5097056a188b71d0b1a
lucianocecere07 <cecereluciano97@gmail.com>
2022-11-15 14:05:30 +0100

    commento esercizio

---
+++ script.js
@@ -0,0 +1,4 @@
+/*Data una lista della spesa già compilata nel vostro codice, stampare in console gli elementi della lista individualmente, prima con un ciclo for, poi con un ciclo while.
+Quando le stampe in console sono corrette, stampare gli elementi in un elenco puntato in HTML, utilizzando uno dei due cicli scritti al punto precedente.*/
+
+
