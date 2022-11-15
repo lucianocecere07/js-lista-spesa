ab78799ff9d2fe7ad5fde391f1594b09e51a2b70
lucianocecere07 <cecereluciano97@gmail.com>
2022-11-15 15:23:42 +0100

    .innerHTML nel loop for

--- script.js
+++ script.js
@@ -6,14 +6,16 @@ let arraySpesa = ["latte", "uova", "mele", "pasta", "arance"];
 
 for (i = 0; i < arraySpesa.length; i++) {
     console.log("elemento da comprare: " + arraySpesa[i]);
+    document.getElementById("elemento").innerHTML = arraySpesa[i];
 }
 
+/*
 document.getElementById("elemento1").innerHTML = arraySpesa[i = 0];
 document.getElementById("elemento2").innerHTML = arraySpesa[i = 1];
 document.getElementById("elemento3").innerHTML = arraySpesa[i = 2];
 document.getElementById("elemento4").innerHTML = arraySpesa[i = 3];
 document.getElementById("elemento5").innerHTML = arraySpesa[i = 4];
-
+*/
 
 /*
 let i = 0;
